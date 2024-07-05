09a3786ed1d4dd6d61a900cc47df9460f49d9f56
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-07-06 02:44:00 +0300

    create my app branch

--- api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Blueprint for API """
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.Tickets import *
-from api.v1.views.Users import *
