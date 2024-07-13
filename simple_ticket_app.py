b8293594becdd6a15cebbb5c01b751df9d0e1da8
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-07-13 13:08:42 +0300

    update simple design

--- simple_ticket_app.py
+++ simple_ticket_app.py
@@ -3,14 +3,21 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
+# @app.route("/")
 @app.route("/home")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Home")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About Us")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact Us")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
